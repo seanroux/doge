6bb0ad7698fdf3e6d61e2ee3cd9fd9a8f9fc146c
Sean Roux <searou96@gmail.com>
2017-01-19 11:26:30 +0200

    Add getWow function

--- src/Doge.php
+++ src/Doge.php
@@ -2,9 +2,19 @@
 
 namespace SeanRoux\Doge\Wow\Such;
 
+use SeanRoux\Wow\Much\Doge\Wow;
+
 class Doge
 {
-  public function getOne() {
-    return '1';
-  }
+    public function getOne()
+    {
+        return '1';
+    }
+
+    public function getWow()
+    {
+        $wow = new Wow();
+
+        return $wow->getWowString('WOW!');
+    }
 }
